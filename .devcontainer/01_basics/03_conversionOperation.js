ebaa9861dca749b520ffbaac8039f1a2bf336325
Mayank Saini <sainimayank7300@gmail.com>
2026-06-03 06:08:22 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abcb"
+console.log(typeof score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false=>0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = -5454
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true
+//"" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
