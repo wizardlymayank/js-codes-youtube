c3352f16c380f7a8f35843f7f3be3f41f7b6f19f
Mayank Saini <sainimayank7300@gmail.com>
2026-06-03 09:27:15 +0000

    "Add JavaScript comparison and type conversion examples"

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,3 +24,14 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter)
