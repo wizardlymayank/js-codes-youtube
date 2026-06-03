e7e2ad347190b6a69aa1d07fa49b628ce947caaf
Mayank Saini <sainimayank7300@gmail.com>
2026-06-03 09:56:09 +0000

    data type summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// Primitive 
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+// Array,Object,Functions
+
+//JavaScript dynamically typed language hai.
+/*Matlab variable declare karte waqt type batani nahi padti, aur ek hi variable ka type baad me change ho sakta hai.
+
+Example:
+
+let data = 10;       // Number
+
+data = "Mayank";     // String
+
+data = true;         // Boolean
+*/
+
+
+
+let myObj ={
+    name: "hitesh",
+    age:22,
+}
+
+const myFunction = function(){
+console.log("Hellow world");
+}
+
+
